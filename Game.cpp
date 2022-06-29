3cd359b3c8465ca2a4b1b73f795c15ba69d1212f
Alex-Cephalopod <76233802+Alex-Cephalopod@users.noreply.github.com>
2022-06-29 16:48:38 -0400

    So far everything works

--- Game.cpp
+++ Game.cpp
@@ -20,12 +20,17 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	brick.width = 10;
-	brick.height = 2;
-	brick.x_position = 0;
-	brick.y_position = 5;
-	brick.doubleThick = true;
-	brick.color = ConsoleColor::DarkGreen;
+	for (int i = 0; i < 5; i++)
+	{
+		Box temp;
+		temp.width = 10;
+		temp.height = 2;
+		temp.x_position = i * Console::WindowWidth() /5;
+		temp.y_position = 5;
+		temp.doubleThick = true;
+		temp.color = ConsoleColor::DarkGreen;
+		bricks.push_back(temp);
+	}
 }
 
 void Game::ResetBall()
@@ -69,7 +74,10 @@ void Game::Render() const
 	ball.Draw();
 
 	// TODO #3 - Update render to render all bricks
-	brick.Draw();
+	for (int i = 0; i < bricks.size(); i++)
+	{
+		bricks[i].Draw();
+	}
 
 	Console::Lock(false);
 }
